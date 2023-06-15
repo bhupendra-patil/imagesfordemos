1238fe3dfe11d7eac6cc829ba59c4476050ee043
bhupendra-patil <bhupendra-patil>
2023-06-15 17:47:42 +0000

    1.47

--- sample2.py
+++ sample2.py
@@ -30,4 +30,104 @@ print(file_contents)
 
 # COMMAND ----------
 
+# Import the required libraries
+from pyspark.sql.functions import from_json, col, to_timestamp
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, TimestampType
+from datetime import datetime, timezone
+
+# Define the schema of the Kafka JSON messages
+schema = StructType([
+    StructField("timestamp", StringType(), True),
+    StructField("field1", DoubleType(), True),
+    StructField("field2", DoubleType(), True),
+    StructField("field3", DoubleType(), True)
+])
+
+# Define the options
+kafka_bootstrap_servers = "<kafka_bootstrap_servers>"
+kafka_topic = "<kafka_topic>"
+checkpoint_location = "/mnt/<delta_location>/"
+
+# Define the Delta table location
+delta_table_path = "/mnt/<delta_location>/"
+
+# Read from Kafka stream and convert to DataFrame
+df = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+    .option("subscribe", kafka_topic) \
+    .option("startingOffsets", "earliest") \
+    .load() \
+    .selectExpr("CAST(value AS STRING)") \
+    .select(from_json(col("value"), schema).alias("json")) \
+    .select("json.*") \
+    .withColumn("timestamp", to_timestamp(col("timestamp"), "yyyy-MM-dd'T'HH:mm:ss'Z'").cast(TimestampType()))
+
+# Write the stream to Delta table
+query = df \
+    .writeStream \
+    .format("delta") \
+    .outputMode("append") \
+    .option("checkpointLocation", checkpoint_location) \
+    .table("db_name.table_name")
+
+# Start the streaming query
+query.start()
+
+# Wait for the query to terminate
+query.awaitTermination()
+
+# COMMAND ----------
+
+# Import the required libraries
+from pyspark.sql.functions import from_json, col, to_timestamp
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, TimestampType
+from datetime import datetime, timezone
+
+# Define the schema of the Kafka JSON messages
+schema = StructType([
+    StructField("timestamp", StringType(), True),
+    StructField("field1", DoubleType(), True),
+    StructField("field2", DoubleType(), True),
+    StructField("field3", DoubleType(), True)
+])
+
+# Define the options
+kafka_bootstrap_servers = "<kafka_bootstrap_servers>"
+kafka_topic = "<kafka_topic>"
+checkpoint_location = "/mnt/<delta_location>/"
+
+# Define the Delta table location
+delta_table_path = "/mnt/<delta_location>/"
+
+# Read from Kafka stream and convert to DataFrame
+df = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+    .option("subscribe", kafka_topic) \
+    .option("startingOffsets", "earliest") \
+    .load() \
+    .selectExpr("CAST(value AS STRING)") \
+    .select(from_json(col("value"), schema).alias("json")) \
+    .select("json.*") \
+    .withColumn("timestamp", to_timestamp(col("timestamp"), "yyyy-MM-dd'T'HH:mm:ss'Z'").cast(TimestampType()))
+
+# Write the stream to Delta table
+query = df \
+    .writeStream \
+    .format("delta") \
+    .outputMode("append") \
+    .option("checkpointLocation", checkpoint_location) \
+    .table("db_name.table_name")
+
+# Start the streaming query
+query.start()
+
+# Wait for the query to terminate
+query.awaitTermination()
+
+# COMMAND ----------
+
 
